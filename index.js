79196ec4cccd3901c4871ef28202110eb4b3e883
dynamic-chain-5255 <ritiguda@gmail.com>
2021-01-11 21:17:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   }
   if (string.toUppercase === string){
-    return "YES INDEED!"
+    return "YES INDEED !"
   }
   if (string === "I love you, Grandma."){
     return "I love you, too."
